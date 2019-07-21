e8f3126f4a40b21d13760e776f9f166e43a9f233
Nicola Halim <staticddq@gmail.com>
2019-07-21 22:47:09 +1000

    UI instruction for blink effect, fixed previous scene not having blink UI

---
+++ Assets/Scripts/AnimTrigger.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class AnimTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator anim = null;
+    [SerializeField] private string animName = "";
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.Play(animName);
+        Destroy(gameObject);
+    }
+}
